24a47222c81791e9747c98e195e73360862d995a
WongDarren <wongdarren1997@gmail.com>
2020-12-23 22:01:46 -0800

    Imported Bulls and Cows game into UE and started command line game

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,24 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess the 4 letter word!"));
+    PrintLine(TEXT("Press enter to continue"));
+    HiddenWord = TEXT("able");
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
+    ClearScreen();
 
+    if (HiddenWord == Input)
+    {
+        PrintLine(Input);
+        PrintLine(TEXT("It matches"));
+    }
+    else
+    {
+        PrintLine(Input);
+        PrintLine(TEXT("It does not match"));
+    }
 }
